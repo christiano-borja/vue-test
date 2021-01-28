ea43aa11280cce560e89b6b8359763ca31f3cf33
Christiano Borja <christiano.barros@aisdigital.com.br>
2021-01-28 00:37:16 -0300

    delete unecessasry code

--- nytimes-test/src/plugins/http.js
+++ nytimes-test/src/plugins/http.js
@@ -1,6 +1,5 @@
 'use strict'
 import axios from 'axios'
-// import authService from '../network/services/auth-service'
 
 const config = {
   baseURL: process.env.VUE_APP_API_BASE_URL,
@@ -13,21 +12,6 @@ const config = {
 
 const http = axios.create(config)
 
-// http.interceptors.request.use(
-//   function (config) {
-//     const accessToken = localStorage.getItem('authToken');
-
-//     if (accessToken) {
-//       config.headers.Authorization = `Bearer ${accessToken}`;
-//     }
-//     // Do something before request is sent
-//     return config
-//   },
-//   function (error) {
-//     // Do something with request error
-//     return Promise.reject(error)
-//   }
-// )
 
 // Add a response interceptor
 http.interceptors.response.use(
